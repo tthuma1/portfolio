7d2577718bdffe2a54a0e3e4946493cbee572038
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2025-10-12 23:37:52 +0200

    fixed grammar

--- app/components/CardAchievements.vue
+++ app/components/CardAchievements.vue
@@ -57,7 +57,7 @@
       <h3 class="mb-2 mt-2 text-lg font-semibold text-center">EuroSkills 2025</h3>
       <p>
         I competed and won at SloSkills 2024 in the ICT
-        category. In September 2025 I competed at
+        category. In September 2025, I competed at
         <a
           href="https://euroskills2025.com/"
           target="_blank"
