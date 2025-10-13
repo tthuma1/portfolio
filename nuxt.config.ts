f844f7af85cc62a0a7fd13e4259e9c8a53af77ea
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2025-10-13 09:45:00 +0200

    moved google fonts css to self host

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,10 +46,7 @@ export default defineNuxtConfig({
         { property: 'og:image:height', content: '1080' },
       ],
       link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: "" },
-        /** https://web.dev/articles/defer-non-critical-css */
-        { href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap', rel: 'preload', as: 'style', onload: "this.onload=null;this.rel='stylesheet'" },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'manifest', href: 'manifest.json' },
         { rel: 'apple-touch-icon', href: 'icons/apple-icon-180.png' },
